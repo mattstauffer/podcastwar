66de6f2990cdc24614b960199aee2fee5ccbc384
Matt Stauffer <matt@tighten.co>
2015-05-26 23:32:40 -0400

    Get basic dice throwing mchanism

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
-alert('woma');
+var DiceGame = {
+    init: function() {
+        var that = this;
 
-function awesome() {
-    // Badly formatted
-    for( i = 1; i<=15; i++)
-    {
-        alert(i);
+        that.$container = $('#container');
+        that.$throwButton = $('#throwButton');
+
+        that.$throwButton.on('click', function() {
+            that.throw();
+        });
+    },
+    throw: function() {
+        this.$container.text(this.getRandomNumber());
+    },
+    live: [],
+    pool: [],
+    getRandomNumber: function() {
+        return Math.round(Math.random() * 5) + 1;
     }
-}
+};
+
+$(function() {
+    DiceGame.init();
+});
