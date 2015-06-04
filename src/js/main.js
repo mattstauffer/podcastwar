82c6e577afda26266a182fbf3869f3610f31a6d6
Matt Stauffer <matt@tighten.co>
2015-06-03 22:48:50 -0400

    Randomization and better less-ificaiton

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
+/* References:
+http://www.trollmystic.com/pub/2012/06/03/javascript-animated-d6-dice-roller-code-released-to-public-domain/
+http://eposic.org/samples/dice/part3.php
+http://codepen.io/SpeauDetcR/pen/nxbCz
+http://codepen.io/tameraydin/pen/CADvB
+*/
+
+var DiceUi = {
+    rollToNumber: function(num) {
+        console.log(num);
+
+        var translate = {
+                1: 'show-back',
+                2: 'show-right',
+                3: 'show-left',
+                4: 'show-top',
+                5: 'show-bottom',
+                6: 'show-front'
+            },
+            $cube = $('#cube');
+
+        $cube.addClass(translate[num]);
+        $cube.removeClass($cube.data('previous-class'));
+        $cube.data('previous-class', translate[num]);
+    }
+};
+
 var DiceGame = {
     init: function() {
         var that = this;
@@ -10,15 +37,33 @@ var DiceGame = {
         });
     },
     throw: function() {
-        this.$container.text(this.getRandomNumber());
+        DiceUi.rollToNumber(this.getRandomNumber());
     },
+    previousRandomNumber: 0,
     live: [],
     pool: [],
     getRandomNumber: function() {
-        return Math.round(Math.random() * 5) + 1;
-    }
+        var num = Math.round(Math.random() * 5) + 1;
+
+        if (num == this.previousRandomNumber) {
+            num = this.getRandomNumber();
+        }
+
+        this.previousRandomNumber = num;
+
+        return num;
+    },
+
 };
 
 $(function() {
     DiceGame.init();
 });
+
+
+$(document).ready(function(){
+    $("a").click(function () {
+        DiceGame.throw();
+    });
+});
+
